6b78a613b0dc1fddf40820ff435a2f7c2f058f55
Jeremy Zheng <jitang.zheng@gmail.com>
2018-03-06 03:08:59 -0800

    init config.toml

--- env/commands.go
+++ env/commands.go
@@ -1,10 +1,115 @@
 package env
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/BurntSushi/toml"
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+)
 
 var commands []cli.Command
 
 // Command register commands
 func Command(args ...cli.Command) {
 	commands = append(commands, args...)
+}
+
+func init() {
+	Command(
+		cli.Command{
+			Name:    "routes",
+			Aliases: []string{"rt"},
+			Usage:   "print out all defined routes",
+			Action: func(_ *cli.Context) error {
+				tpl := "%-16s %s\n"
+				fmt.Printf(tpl, "METHODS", "PATH")
+				return nil
+			},
+		},
+		cli.Command{
+			Name:    "server",
+			Aliases: []string{"s"},
+			Usage:   "start the app server",
+			Action: func(_ *cli.Context) error {
+				return nil
+			},
+		},
+		cli.Command{
+			Name:    "init",
+			Aliases: []string{"i"},
+			Usage:   "init config files",
+			Action: func(_ *cli.Context) error {
+				if err := os.MkdirAll(ROOT, 0755); err != nil {
+					return err
+				}
+				// config.toml
+				if err := generateConfigToml(); err != nil {
+					return err
+				}
+				return nil
+			},
+		},
+	)
+}
+
+func generateConfigToml() error {
+	fn := path.Join(ROOT, "config.toml")
+	log.Infof("generate file %s\n", fn)
+	fd, err := os.OpenFile(fn, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0600)
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+
+	var header []Link
+	for i := 1; i <= 6; i++ {
+		var it Link
+		it.Title = fmt.Sprintf("header %d", i)
+		it.URL = fmt.Sprintf("footer %d", i)
+		for j := 1; j <= 3; j++ {
+			it.Children = append(
+				it.Children,
+				Link{
+					Title: fmt.Sprintf("header %d %d", i, j),
+					URL:   fmt.Sprintf("/header-%d-%d", i, j),
+				},
+			)
+		}
+		header = append(header, it)
+	}
+	var footer []Link
+	for i := 1; i <= 5; i++ {
+		footer = append(
+			footer,
+			Link{
+				Title: fmt.Sprintf("footer %d", i),
+				URL:   fmt.Sprintf("/footer-%d", i),
+			},
+		)
+	}
+	return toml.NewEncoder(fd).Encode(Config{
+		Port:           8080,
+		Theme:          "bootstrap",
+		Administrators: []string{"change-me@localhost"},
+		ReCaptcha: ReCaptcha{
+			SiteKey:   "reCAPTCHA-site-key",
+			SecretKey: "reCAPTCHA-secret-key",
+		},
+		SMTP: SMTP{
+			Host:     "smtp.gmail.com",
+			Port:     465,
+			User:     "who-am-i@gmail.com",
+			Password: "change-me",
+		},
+		Site: map[string]string{
+			"title":       "site title",
+			"subhead":     "site subhead",
+			"description": "site description",
+		},
+		Header: header,
+		Footer: footer,
+	})
 }
--- env/config.go
+++ env/config.go
@@ -1,16 +1,39 @@
 package env
 
+const (
+	// ROOT root dir
+	ROOT = "tmp"
+)
+
 // SMTP smtp
 type SMTP struct {
-	User     string
-	Password string
-	Port     string
-	Host     string
+	User     string `toml:"user"`
+	Password string `toml:"password"`
+	Host     string `toml:"host"`
+	Port     int    `toml:"port"`
 }
 
 // Config configuration model
 type Config struct {
-	Port           int
-	Theme          string
-	Administrators []string
+	Port           int               `toml:"port"`
+	Theme          string            `toml:"theme"`
+	Administrators []string          `toml:"administrators"`
+	ReCaptcha      ReCaptcha         `toml:"recaptcha"`
+	SMTP           SMTP              `toml:"smtp"`
+	Site           map[string]string `toml:"site"`
+	Header         []Link            `toml:"header"`
+	Footer         []Link            `toml:"footer"`
+}
+
+// ReCaptcha https://www.google.com/recaptcha/intro/
+type ReCaptcha struct {
+	SiteKey   string `toml:"site-key"`
+	SecretKey string `toml:"secret-key"`
+}
+
+// Link link
+type Link struct {
+	Title    string `toml:"title"`
+	URL      string `toml:"url"`
+	Children []Link `toml:"children"`
 }
